4051a2c0bf7fdc74dfc1fd70fa53269a7f0448d8
hawariKv <abdulla@keyvalue.systems>
2022-03-31 17:18:47 +0530

    readme changes

--- src/app/util/rest/request.ts
+++ src/app/util/rest/request.ts
@@ -4,9 +4,6 @@ import { Request } from "express";
  * Interface to add extra modifiers to request.
  */
 export default interface RequestWithUser extends Request {
-  // To use userId and role, please inject the same in a middleware, by decoding an access token.
-  userId: string;
-  role: string;
   startTime?: number;
   userAgent?: { [key: string]: any };
 }
